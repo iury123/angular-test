73ba487b51b54db28ba90304400ebadbdc828ae6
Iury da Rocha Miguel <iuryrocha@inatel.br>
2020-04-23 20:49:33 -0300

    commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { LoginComponent } from './login/login.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+const routes: Routes = [
+  { path: 'login', loadChildren: () => import('./login/login.module').then(m => m.LoginModule) },
+  { path: '', pathMatch: 'full', redirectTo: '/login' },
+  { path: '**', component: LoginComponent },
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+})
+export class AppRoutingModule { }
---
+++ src/app/login/login.module.ts
@@ -0,0 +1,9 @@
+import { LoginComponent } from './login.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@NgModule({
+  declarations: [LoginComponent],
+  imports: [CommonModule],
+})
+export class LoginModule {}
