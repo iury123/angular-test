d45e8083c86c5b41aa2814a9637c5e3790d6c99f
Iury da Rocha Miguel <iuryrocha@inatel.br>
2020-04-26 15:22:13 -0300

    retirando imports desnecessarios

--- src/app/widgets/widgets.module.ts
+++ src/app/widgets/widgets.module.ts
@@ -1,6 +1,4 @@
-import { AppRoutingModule } from './../app-routing.module';
 import { HomeRoutingModule } from './../home/home-routing.module';
-import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
